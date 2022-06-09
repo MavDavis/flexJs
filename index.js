464290e65f0b4001d6dc05be463d72f5cc01a386
MavDavis <ekwesimarvelous@gmail.com>
2022-06-09 12:32:56 +0100

    style commit

--- index.js
+++ index.js
@@ -7,13 +7,18 @@ fetch("members.json")
 function ZeusTask(array) {
     let users = "";
     array.forEach((mem) => {
-        users += ` <div class="each-mem">
-                                <div class="each-mem-name">
-                                <p class="color">${mem.firstname} ${mem.lastname}</p>
-                                <img src=${mem.img} alt=${mem.l}>
-                                </div>
-                                
-                            </div>`;
+        users += `
+         <div class="each-mem-name">            <img src=${mem.img} alt=${mem.l}><div class="text">
+         <h4 class=""> ${mem.firstname} ${mem.lastname} </h4>
+<p class="">BestMovie: ${mem.bestmovie} </p>
+
+<p class="">Complexion: ${mem.complexion} 
+<p class=""> Birthmonth: ${mem.birthmonth} </p>
+<p class="">Age: ${mem.Age} </p>
+<p class="">State: ${mem.state} </p>
+         </div>
+
+</div>`;
     });
 
     document.getElementById("app").innerHTML = users;
